36cddb3398cde841118e019655bcba9521532e34
Sebastian <s.max@koeln.de>
2021-02-07 09:33:18 +0100

    change

--- update.js
+++ update.js
@@ -1,12 +1,12 @@
 function update() {
+    console.log("updating...");
     $.ajax({
         type: "GET",
         url: "https://api.openweathermap.org/data/2.5/weather?zip=50737,DE&appid=02ee4bca37e17064be5008c73ff486ce&lang=de"
     }).done(function (data) {
         var temp = (data.main.temp - 273.15).toFixed(1) + "°C";
         $("#current_temperature").text(temp);
-        $("#current_icon").removeClass();
-        $("#current_icon").addClass("wi wi-owm-" + data.weather[0].id);
+        $("#current_icon").attr('class', "wi wi-owm-" + data.weather[0].id);
     
         var wind_direction = get_wind_direction(data.wind.deg);
         var wind_speed = (data.wind.speed*3.6).toFixed(1);
@@ -19,20 +19,17 @@ function update() {
     }).done(function (data) {
         var prediction_temp = (data.list[0].main.temp - 273.15).toFixed(0) + "°C";
         $("#prediction_1").text(prediction_temp);
-        $("#prediction_icon_1").removeClass();
-        $("#prediction_icon_1").addClass("wi wi-owm-" + data.list[0].weather[0].id);
+        $("#prediction_icon_1").attr('class', "wi wi-owm-" + data.list[0].weather[0].id);
         $("#prediction_rain_1").text(data.list[0].pop * 100 + "%");
     
         prediction_temp = (data.list[1].main.temp - 273.15).toFixed(0) + "°C";
         $("#prediction_2").text(prediction_temp);
-        $("#prediction_icon_2").removeClass();
-        $("#prediction_icon_2").addClass("wi wi-owm-" + data.list[1].weather[0].id);
+        $("#prediction_icon_2").attr('class', "wi wi-owm-" + data.list[1].weather[0].id);
         $("#prediction_rain_2").text(data.list[1].pop * 100 + "%");
     
         prediction_temp = (data.list[2].main.temp - 273.15).toFixed(0) + "°C";
         $("#prediction_3").text(prediction_temp);
-        $("#prediction_icon_3").removeClass();
-        $("#prediction_icon_3").addClass("wi wi-owm-" + data.list[2].weather[0].id);
+        $("#prediction_icon_3").attr('class', "wi wi-owm-" + data.list[2].weather[0].id);
         $("#prediction_rain_3").text(data.list[2].pop * 100 + "%");
     });
 }
