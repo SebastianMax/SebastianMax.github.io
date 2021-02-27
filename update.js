7ef6e41e729ac2786ad668508e82577414b1affd
Sebastian <s.max@koeln.de>
2021-02-27 10:21:48 +0100

    zero

--- update.js
+++ update.js
@@ -1,5 +1,4 @@
 function update() {
-    console.log("update");
     $.ajax({
         type: "GET",
         url: "https://api.openweathermap.org/data/2.5/weather?zip=50737,DE&appid=02ee4bca37e17064be5008c73ff486ce&lang=de"
@@ -13,13 +12,17 @@ function update() {
         $("#current_wind").text(wind_speed + "km/h (" + wind_direction + ")");
 
         var sunrise = new Date(data.sys.sunrise * 1000 + data.timezone * 1000);
+        var sunrise_h = sunrise.getHours();
+        var sunrise_min = sunrise.getMinutes() < 10 ? "0" + sunrise.getMinutes() : sunrise.getMinutes();
         var sunset = new Date(data.sys.sunset * 1000 + data.timezone * 1000);
+        var sunset_h = sunset.getHours();
+        var sunset_min = sunset.getMinutes() < 10 ? "0" + sunset.getMinutes() : sunset.getMinutes();
+        $("#sunrise").text(sunrise_h + ":" + sunrise_min);
+        $("#sunset").text(sunset_h + ":" + sunset_min);
+
         var daylight = sunset - sunrise;
         var daylight_h = Math.floor(daylight / 1000 / 60 / 60);
         var daylight_min = Math.floor(daylight / 1000 / 60 % 60);
-
-        $("#sunrise").text(sunrise.getHours() + ":" + sunrise.getMinutes());
-        $("#sunset").text(sunset.getHours() + ":" + sunset.getMinutes());
         $("#daylight").text(daylight_h + "h " + daylight_min + "min");
     });
     
